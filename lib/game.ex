66a069590a325d3e9bca2e0f9f814603b582f491
Edison Mendez <efmcuiti@gmail.com>
2019-03-13 23:24:31 -0500

    Add wrappers plus initializer

--- lib/game.ex
+++ lib/game.ex
@@ -1,5 +1,18 @@
 defmodule IslandsEngine.Game do
   use GenServer
+  alias IslandsEngine.{Game, Player}
+  
+  defstruct player1: :none, player2: :none
+
+  def start_link(name) when not is_nil name do
+    GenServer.start_link(__MODULE__, name)
+  end
+
+  def init(name) do
+    {:ok, player1} = Player.start_link(name)
+    {:ok, player2} = Player.start_link()
+    {:ok, %Game{player1: player1, player2: player2}}
+  end
 
   def handle_call(:demo, _from, state) do
     {:reply, state, state}
