1955dd02c1b511f8d26205c0bed0e02ef2c12d06
Edison Mendez <efmcuiti@gmail.com>
2019-03-25 23:00:59 -0500

    Finish the third chapter

--- lib/game.ex
+++ lib/game.ex
@@ -1,8 +1,12 @@
 defmodule IslandsEngine.Game do
   use GenServer
-  alias IslandsEngine.{Game, Player}
+  alias IslandsEngine.{Coordinate, Game, Player}
   
   defstruct player1: :none, player2: :none
+
+  def start_link(name) when is_binary(name) and byte_size(name) > 0 do
+    GenServer.start_link(__MODULE__, name, name: {:global, "game:#{name}"})
+  end
 
   def start_link(name) when not is_nil name do
     GenServer.start_link(__MODULE__, name)
@@ -14,11 +18,83 @@ defmodule IslandsEngine.Game do
     {:ok, %Game{player1: player1, player2: player2}}
   end
 
+  def stop(pid) do
+    GenServer.cast(pid, :stop)
+  end
+
+  def handle_cast(:stop, state) do
+    {:stop, :normal, state}
+  end
+
   def handle_call(:demo, _from, state) do
     {:reply, state, state}
   end
 
   def call_demo(game) do
     GenServer.call(game, :demo)
+  end
+
+  def add_player(pid, name) when name != nil do
+    GenServer.call(pid, {:add_player, name})
+  end
+
+  def handle_call({:add_player, name}, _from, state) do
+    Player.set_name(state.player2, name)
+    {:reply, :ok, state}
+  end
+
+  def set_island_coordinates(pid, player, island, coordinates)
+    when is_atom player and is_atom island do
+
+    GenServer.call(pid, {:set_island_coordinates, player, island, coordinates})
+  end
+
+  def handle_call({:set_island_coordinates, player, island, coordinates}, _from, state) do
+    state
+    |> Map.get(player)
+    |> Player.set_island_coordinates(island, coordinates)
+    {:reply, :ok, state}
+  end
+
+  def guess_coordinate(pid, player, coordinate) when is_atom player and is_atom coordinate do
+    GenServer.call(pid, {:guess, player, coordinate})
+  end
+
+  def handle_call({:guess, player, coordinate}, _from, state) do
+    oponent = oponent(state, player)
+    oponent_board = Player.get_board(oponent)
+    Player.guess_coordinate(oponent_board, coordinate)
+    |> forest_check(oponent, coordinate)
+    |> win_check(oponent, state)
+  end
+
+  defp forest_check(:miss, _oponent, _coordinate) do
+    {:miss, :none}
+  end
+  
+  defp forest_check(:hit, oponent, coordinate) do
+    island_key = Player.forested_island(oponent, coordinate)
+    {:hit, island_key}
+  end
+
+  defp win_check({hit_or_miss, :none}, _oponent, state) do
+    {:reply, {hit_or_miss, :none, :no_win}, state}
+  end
+
+  defp win_check({:hit, island_key}, oponent, state) do
+    win_status =
+      case Player.win?(oponent) do
+        true -> :win
+        false -> :no_win
+      end
+    {:reply, {:hit, island_key, win_status}, state}
+  end
+
+  defp oponent(state, :player1) do
+    state.player2
+  end
+
+  defp oponent(state, _player2) do
+    state.player1
   end
 end
